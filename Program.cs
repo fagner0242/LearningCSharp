f080da007a325b72269f736f929bd748143bedf1
Fagner <eufagner0@gmail.com>
2023-07-11 21:59:24 -0300

    studying List and lambda expressions

--- Program.cs
+++ Program.cs
@@ -15,9 +15,32 @@ class Program
         names.Add("Emmet");
         names.Add("Brown");
 
+        names.Insert(2, "Marco");
+
         foreach (string namesToShow in names)
         {
             Console.WriteLine(namesToShow);
         }
+
+        Console.WriteLine($"List count: {names.Count}");
+
+        Console.WriteLine("--------------------------");
+
+        string findE = names.Find(x => x[0] == 'E')!;
+        Console.WriteLine($"First name with E: {findE}");
+
+        Console.WriteLine();
+
+        string findLastA = names.FindLast(x => x[0] == 'A')!;
+        Console.WriteLine($"Last name with A");
+
+        int findFirstB = names.FindIndex(x => x[0] == 'B');
+
+        Console.WriteLine($"First name index with B: {findFirstB}");
+    }
+
+    static bool Test(string findA)
+    {
+        return findA[0] == 'E';
     }
 }
