cc7bed5f84c34175de95db29b032d10bea7fcf2a
Fagner <eufagner0@gmail.com>
2023-07-13 07:23:27 -0300

    Estudando matriz

--- Program.cs
+++ Program.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Globalization;
 using System.Collections.Generic;
-using LearningCSharp.Entities;
 
 namespace LearningCSharp;
 
@@ -9,45 +8,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.Write("How many employees will be registered? ");
-        int numberOfEmployees = int.Parse(Console.ReadLine()!);
+        double[,] mat = new double[2,3];
 
-        List<Employee> employees = new List<Employee>();
+        Console.WriteLine(mat.Length);
 
-        for (int i = 1; i <= numberOfEmployees; i++)
-        {
-            Console.WriteLine($"Employee #{i}:");
+        Console.WriteLine(mat.Rank);
 
-            Console.Write("Id: ");
-            int id = int.Parse(Console.ReadLine()!);
-
-            Console.Write("Name: ");
-            string name = Console.ReadLine()!;
-
-            Console.Write("Salary: ");
-            double salary = double.Parse(Console.ReadLine()!, CultureInfo.InvariantCulture);
-
-            Console.WriteLine();
-
-            employees.Add(new Employee(id, name, salary));
-        }
-
-        Console.Write("Enter the employee id that will have salary increase: ");
-        int employeeIdForIncrease = int.Parse(Console.ReadLine()!);
-
-        Employee employee = employees.Find(x => x.Id == employeeIdForIncrease)!;
-        if (employee is not null)
-        {
-            Console.Write("Enter the percentage: ");
-            double percentage = double.Parse(Console.ReadLine()!, CultureInfo.InvariantCulture);
-            employee.SalaryIncrease(percentage);
-        }
-
-        Console.WriteLine("Updated list of employees:");
-
-        for (int i = 0; i < numberOfEmployees; i++)
-        {
-            Console.WriteLine(employees[i]);
-        }
+        Console.WriteLine(mat.GetLength(1));
     }
 }
