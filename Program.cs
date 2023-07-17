9d63d33c4c9f3efe6e62e18194e410e673459886
Fagner <eufagner0@gmail.com>
2023-07-17 19:27:53 -0300

    Operador de coalescência nula

--- Program.cs
+++ Program.cs
@@ -9,39 +9,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        var x = int.Parse(Console.ReadLine()!);
+        int? x = null;
+        int? y = 5;
 
-        string day = string.Empty;
+        int a = x ?? 10;
+        int b = y ?? 10;
 
-        switch (x)
-        {
-            case 1:
-                day = "Sunday";
-                break;
-            case 2:
-                day = "Monday";
-                break;
-            case 3:
-                day = "Tuesday";
-                break;
-            case 4:
-                day = "Wednesday";
-                break;
-            case 5:
-                day = "Thursday";
-                break;
-            case 6:
-                day = "Friday";
-                break;
-            case 7:
-                day = "Saturday";
-                break;
-
-            default:
-                Console.WriteLine("Only 1 to 7");
-                break;
-        }
-
-        Console.WriteLine(day);
+        Console.WriteLine(a);
+        Console.WriteLine(b);
     }
 }
