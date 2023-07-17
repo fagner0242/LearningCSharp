91633dcd74d208adf8f1785aeefeb26cd6837b7a
Fagner <eufagner0@gmail.com>
2023-07-17 19:24:58 -0300

    using switch case instead of if else, and type var inference

--- Program.cs
+++ Program.cs
@@ -9,6 +9,39 @@ class Program
 {
     static void Main(string[] args)
     {
-        
+        int x = int.Parse(Console.ReadLine()!);
+
+        string day = string.Empty;
+
+        switch (x)
+        {
+            case 1:
+                day = "Sunday";
+                break;
+            case 2:
+                day = "Monday";
+                break;
+            case 3:
+                day = "Tuesday";
+                break;
+            case 4:
+                day = "Wednesday";
+                break;
+            case 5:
+                day = "Thursday";
+                break;
+            case 6:
+                day = "Friday";
+                break;
+            case 7:
+                day = "Saturday";
+                break;
+
+            default:
+                Console.WriteLine("Only 1 to 7");
+                break;
+        }
+
+        Console.WriteLine(day);
     }
 }
