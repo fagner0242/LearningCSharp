ca512c1be65a9ee97032366718cdf8d062e96d6c
Fagner <eufagner0@gmail.com>
2023-07-21 07:35:57 -0300

    Formatações com ParseExact

--- Program.cs
+++ Program.cs
@@ -9,15 +9,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        DateTime d1 = DateTime.Parse("2000-08-15");
-        DateTime d2 = DateTime.Parse("2000-08-15 13:05:58");
+        DateTime d1 = DateTime.ParseExact("2000-08-15", "yyyy-MM-dd", CultureInfo.InvariantCulture);
+        DateTime d2 = DateTime.ParseExact("2000-08-15 13:05:58", "yyyy-MM-dd HH:mm:ss", CultureInfo.InvariantCulture);
 
-        DateTime d3 = DateTime.Parse("15/08/2000");
-        DateTime d4 = DateTime.Parse("15/08/2000 13:05:58");
+        DateTime d3 = DateTime.ParseExact("15/08/2000 13:05:58", "dd/MM/yyyy HH:mm:ss", CultureInfo.InvariantCulture);
+
 
         Console.WriteLine(d1);
         Console.WriteLine(d2);
         Console.WriteLine(d3);
-        Console.WriteLine(d4);
     }
 }
