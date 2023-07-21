223eeeb73b0a1ef3377c1019c2ce92dcc0456fd6
Fagner <eufagner0@gmail.com>
2023-07-21 07:15:48 -0300

    Converting string to date and time

--- Program.cs
+++ Program.cs
@@ -9,20 +9,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        DateTime dateTime = new DateTime(2023, 07, 20);
-        DateTime dateTime2 = new DateTime(2023, 07, 20, 0, 1, 2);
-        DateTime dateTime3 = new DateTime(2023, 07, 20, 0, 1, 2, 500);
+        DateTime d1 = DateTime.Parse("2000-08-15");
+        DateTime d2 = DateTime.Parse("2000-08-15 13:05:58");
 
-        DateTime dateTime4 = DateTime.Now;
-        DateTime dateTime5 = DateTime.Today;
-        DateTime dateTime6 = DateTime.UtcNow;
+        DateTime d3 = DateTime.Parse("15/08/2000");
+        DateTime d4 = DateTime.Parse("15/08/2000 13:05:58");
 
-
-        Console.WriteLine(dateTime);
-        Console.WriteLine(dateTime2);
-        Console.WriteLine(dateTime3);
-        Console.WriteLine(dateTime4);
-        Console.WriteLine(dateTime5);
-        Console.WriteLine(dateTime6);
+        Console.WriteLine(d1);
+        Console.WriteLine(d2);
+        Console.WriteLine(d3);
+        Console.WriteLine(d4);
     }
 }
