2c3f473d1adfe3d4cc5b868d6555bdc4de475497
Fagner <eufagner0@gmail.com>
2023-07-20 22:18:30 -0300

    DateTime diferentes formatações

--- Program.cs
+++ Program.cs
@@ -9,9 +9,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        DateTime d1 = DateTime.Now;
+        DateTime dateTime = new DateTime(2023, 07, 20);
+        DateTime dateTime2 = new DateTime(2023, 07, 20, 0, 1, 2);
+        DateTime dateTime3 = new DateTime(2023, 07, 20, 0, 1, 2, 500);
 
-        Console.WriteLine(d1);
-        Console.WriteLine(d1.Ticks);
+        DateTime dateTime4 = DateTime.Now;
+        DateTime dateTime5 = DateTime.Today;
+        DateTime dateTime6 = DateTime.UtcNow;
+
+
+        Console.WriteLine(dateTime);
+        Console.WriteLine(dateTime2);
+        Console.WriteLine(dateTime3);
+        Console.WriteLine(dateTime4);
+        Console.WriteLine(dateTime5);
+        Console.WriteLine(dateTime6);
     }
 }
