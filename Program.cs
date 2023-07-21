547aa6f0480af579ca276a464dd0c83c13cb1129
Fagner <eufagner0@gmail.com>
2023-07-20 21:58:08 -0300

    Starting a new study

--- Program.cs
+++ Program.cs
@@ -9,39 +9,10 @@ class Program
 {
     static void Main(string[] args)
     {
-        string original = "abcde FGHIJ ABC abc DEFG   ";
+        string test = "abcd efghijk lmno";
 
-        string originalToUpper = original.ToUpper();
-        string originalToLower = original.ToLower();
-        string originalTrimmed = original.Trim();
+        string indexTest = test[3..8];
 
-        int originalSearching = original.IndexOf("bc");
-        int originalSearchingLast = original.LastIndexOf("bc");
-
-        string originalSubstring = original.Substring(3);
-        //string originalSubstring = original[3..];
-        //Using range operator!!!
-
-        //string originalSubstringLength = original.Substring(3, 5);
-        string originalSubstringLength = original[3..5];
-
-        string originalCharReplaced = original.Replace('a', 'x');
-        string originalStringReplaced = original.Replace("abc", "xyz");
-
-        bool nullTest = String.IsNullOrEmpty(original);
-        bool nullTest2 = String.IsNullOrWhiteSpace(original);
-
-        Console.WriteLine($"Original: -{original}-");
-        Console.WriteLine($"To Upper: -{originalToUpper}-");
-        Console.WriteLine($"To Lower: -{originalToLower}-");
-        Console.WriteLine($"Trim: -{originalTrimmed}-");
-        Console.WriteLine($"Index of ('bc'): -{originalSearching}-");
-        Console.WriteLine($"Last index of ('bc'): -{originalSearchingLast}-");
-        Console.WriteLine($"Substring(3): -{originalSubstring}-");
-        Console.WriteLine($"SubstringLength(3, 5): -{originalSubstringLength}-");
-        Console.WriteLine($"Replace('a', 'x': -{originalCharReplaced}-)");
-        Console.WriteLine($"Replace('abc', 'xyz')-{originalStringReplaced}-");
-        Console.WriteLine($"Is null or empty: {nullTest}");
-        Console.WriteLine($"Is null or white space: {nullTest2}");
+        Console.WriteLine(indexTest);
     }
 }
