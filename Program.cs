4d4f5ff7418e413d1857022f322d4fa2d01677fe
Fagner <eufagner0@gmail.com>
2023-07-25 09:05:47 -0300

    TimeSpan Properties

--- Program.cs
+++ Program.cs
@@ -9,13 +9,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        DateTime d1 = DateTime.Now;
+        TimeSpan t = new TimeSpan(2, 3, 5, 7, 11);
 
-        DateTime d2 = d1.AddDays(3);
+        TimeSpan t1 = TimeSpan.MaxValue;
+        TimeSpan t2 = TimeSpan.MinValue;
+        TimeSpan t3 = TimeSpan.Zero;
 
-        TimeSpan difference = d2.Subtract(d1);
+        Console.WriteLine(t);
+        Console.WriteLine(t1);
+        Console.WriteLine(t2);
+        Console.WriteLine(t3);
 
-        Console.WriteLine(difference);
+        Console.WriteLine($"Days: {t.Days}");
+        Console.WriteLine($"Hours: {t.Hours}");
+        Console.WriteLine($"Minutes: {t.Minutes}");
+        Console.WriteLine($"MIlliseconds: {t.Milliseconds}");
+        Console.WriteLine($"Seconds: {t.Seconds}");
+        Console.WriteLine($"Ticks: {t.Ticks}");
+
+        Console.WriteLine($"TotalDays: {t.TotalDays}");
+        Console.WriteLine($"TotalHours: {t.TotalHours}");
+        Console.WriteLine($"TotalMinutes: {t.TotalMinutes}");
+        Console.WriteLine($"TotalSeconds: {t.TotalSeconds}");
+        Console.WriteLine($"TotalMilliseconds: {t.TotalMilliseconds}");
 
     }
 }
