73135015add69236ed3c1dd38e424b64eb72cae4
Fagner <eufagner0@gmail.com>
2023-07-26 22:02:55 -0300

    Different types of DateTime

--- Program.cs
+++ Program.cs
@@ -9,15 +9,20 @@ class Program
 {
     static void Main(string[] args)
     {
+        DateTime dateTime = DateTime.Now;
+
         DateTime d1 = new DateTime(2000, 8, 15, 13, 5, 58, DateTimeKind.Local);
 
         DateTime d2 = new DateTime(2000, 8, 15, 13, 5, 58, DateTimeKind.Utc);
 
-        DateTime d3 = new DateTime(2000, 8, 15, 13, 5, 58);
+        Console.WriteLine($"d1: {d1}");
+        Console.WriteLine($"d1 Kind: {d1.Kind}");
+        Console.WriteLine($"d1 To Local: {d1.ToLocalTime()}");
+        Console.WriteLine($"d1 To Utc: {d1.ToUniversalTime()}");
 
+        Console.WriteLine(dateTime);
+        Console.WriteLine(dateTime.ToUniversalTime());
 
-        Console.WriteLine(d1);
         Console.WriteLine(d2);
-        Console.WriteLine(d3);
     }
 }
