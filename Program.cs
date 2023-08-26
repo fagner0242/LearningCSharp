e922f782001be330ed840458366b02494009c35a
Fagner <eufagner0@gmail.com>
2023-08-25 21:33:41 -0300

    Herança

--- Program.cs
+++ Program.cs
@@ -1,5 +1,4 @@
 ﻿using LearningCSharp.Entities;
-using LearningCSharp.Entities.Enums;
 using System;
 using System.Globalization;
 using System.Reflection.Metadata.Ecma335;
@@ -10,48 +9,8 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Enter client data:");
+        BusinessAccount account = new(8010, "Maria", 500.50, 5000.0);
 
-        Console.Write("Name: ");
-        string name = Console.ReadLine()!;
-
-        Console.Write("Email: ");
-        string email = Console.ReadLine()!;
-
-        Console.Write("Birth date (DD/MM/YYYY): ");
-        DateTime birthDate = DateTime.Parse(Console.ReadLine()!);
-
-        Client client = new Client(name, email, birthDate);
-
-        Console.WriteLine("Enter order data:");
-
-        Console.Write("Status: ");
-        OrderStatus status = Enum.Parse<OrderStatus>(Console.ReadLine()!);
-
-        Order order = new(DateTime.Now, status, client);
-
-        Console.Write("How many items to this order? ");
-        int numberOfItems = int.Parse(Console.ReadLine()!);
-
-        for (int i = 1; i <= numberOfItems; i++)
-        {
-            Console.Write("Product name: ");
-            string productName = Console.ReadLine()!;
-
-            Console.Write("Product price: ");
-            double productPrice = double.Parse(Console.ReadLine()!, CultureInfo.InvariantCulture);
-
-            Console.WriteLine("Quantity: ");
-            int quantity = int.Parse(Console.ReadLine()!);
-
-            Product product = new(productName, productPrice);
-
-            OrderItem items = new(quantity, productPrice, product);
-
-            order.AddItem(items);
-        }
-
-        Console.WriteLine("ORDER SUMMARY:");
-        Console.WriteLine(order);
+        Console.WriteLine(account.Balance);
     }
 }
