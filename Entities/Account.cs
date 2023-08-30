c672fd1c3ed5602fc86144324ddda09bebfaa859
Fagner <eufagner0@gmail.com>
2023-08-29 22:10:53 -0300

    Sobreposição de classe e método, com (virtual e override)

--- Entities/Account.cs
+++ Entities/Account.cs
@@ -19,9 +19,9 @@ internal class Account
         Balance = balance;
     }
 
-    public void WithDraw(double amount)
+    public virtual void WithDraw(double amount)
     {
-        Balance -= amount;
+        Balance -= amount + 5.0;
     }
 
     public void Deposit(double amount)
--- Program.cs
+++ Program.cs
@@ -9,37 +9,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Account acc = new Account(1001, "Alex", 0.0);
+        Account mainAccount = new(1001, "Alex", 500.0);
+        Account savingsAccount = new SavingsAccount(1002, "Alex", 500.0, 0.01);
 
-        BusinessAccount bacc = new BusinessAccount(1002, "Maria", 0.0, 500.0);
+        mainAccount.WithDraw(10.0);
+        savingsAccount.WithDraw(10.0);
 
-        //UPCASTING 
-
-        Account acc1 = bacc;
-        Account acc2 = new BusinessAccount(1003, "Bob", 0.0, 200.0);
-        Account acc3 = new SavingsAccount(1003, "Bob", 0.0, 0.01);
-
-        //DOWNCASTING
-
-        BusinessAccount acc4 = acc2 as BusinessAccount;
-        if (acc4 is not null)
-        {
-            acc4.Loan(150.0);
-            Console.WriteLine(acc4.Balance);
-        }
-        
-        if (acc3 is BusinessAccount)
-        {
-            BusinessAccount acc5 = (BusinessAccount)acc3;
-            acc5.Loan(200.0);
-            Console.WriteLine("Loan!");
-        }
-
-        if (acc3 is SavingsAccount)
-        {
-            SavingsAccount acc5 = acc3 as SavingsAccount;
-            acc5.UpdateBalance();
-            Console.WriteLine("Update!");
-        }
+        Console.WriteLine(mainAccount.Balance);
+        Console.WriteLine(savingsAccount.Balance);  
     }
 } 
